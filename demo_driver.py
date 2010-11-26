22230a3b3ec384211209eb4c5e6b9d4ec3166a00
Anders Eurenius <aes@nerdshack.com>
2010-11-27 00:29:34 +0100

    Adding reading from other demo file, if filename supplied

--- demo_driver.py
+++ demo_driver.py
@@ -2,8 +2,12 @@ import sys
 from curses_menu import CursesGuard, Menu
 
 if __name__ == '__main__': # {{{
-    with file('words6.txt') as f:
-        l = sum([ x.split() for x in f.readlines() ],[])
+    if len(sys.argv) > 1:
+        with file(sys.argv[1]) as f:
+            l = f.readlines()
+    else:
+        with file('words6.txt') as f:
+            l = sum([ x.split() for x in f.readlines() ],[])
     with CursesGuard() as window:
         m = Menu(window, l or ['abacus','binary','cipher'])
         while isinstance(m, Menu): m = m.run()
